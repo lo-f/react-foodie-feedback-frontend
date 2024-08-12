738142355ecfd6f5d0d58acece589fc2f0418215
Cody Kremer <ckremer99@outlook.com>
2024-08-12 12:25:43 -0700

    working on form

--- src/components/RestaurantsList/RestaurantsList.jsx
+++ src/components/RestaurantsList/RestaurantsList.jsx
@@ -5,16 +5,15 @@ const RestaurantsList = (props) => {
         <>
         <h1>Restaurants</h1>
         <main>
-            {props.restaurants.map((restaurant) => (
-                <Link key={restaurant._id} to={`/restaurants/${restaurant._id}`}>
+            {props.restaurants.map((restaurant) => {
+                return <Link key={restaurant._id} to={`/restaurants/${restaurant._id}`}>
                     <header>
                         <div>
                             <h2>{restaurant.name}</h2>
                         </div>
                     </header>
                 </Link>
-
-            ))}
+            })}
         </main>
         </>
     )
---
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const ReviewForm = () => {
+    const [review, setReview] = useState({
+        rating: '',
+        text: '',
+    });
+    
+
+    const handleChange(event) {
+
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Handle form submission logic here
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='rating'>Rating: </label>
+            <select 
+                value={review.rating} 
+                onChange={handleChange} 
+                id='rating'
+                name='rating'
+                className='reviewFormField'>
+                    <option value="">Select a rating</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+            </select>
+            <br />
+            <label htmlFor='text'>Write your review: </label>
+            <textarea 
+                value={review.text} 
+                onChange={handleChange}
+                id='text'
+                name='text'
+                className='reviewFormField' />
+            <br />
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default ReviewForm;
