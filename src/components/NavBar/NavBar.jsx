a4892ffd598e9740234007bfd05e0d44c24ec2a5
lo-franky <lo.frankyl@gmail.com>
2024-08-12 10:23:50 -0500

    added component directories

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,6 +7,8 @@ const NavBar = ({ user, handleSignout }) => {
         <nav>
           <ul>
             <li><Link to="/">Home</Link></li>
+            <li><Link to="/myreviews</li>">My Reviews</Link></li>
+            <li><Link to="/restaurants">Restaurants</Link></li>
             <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
           </ul>
         </nav>
