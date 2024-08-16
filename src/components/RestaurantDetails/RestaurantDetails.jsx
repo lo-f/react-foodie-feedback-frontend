4bfabfa7271f23ada72d91758b5ec914913f0265
lo-franky <lo.frankyl@gmail.com>
2024-08-15 20:31:47 -0500

    added alt text

--- src/components/RestaurantDetails/RestaurantDetails.jsx
+++ src/components/RestaurantDetails/RestaurantDetails.jsx
@@ -51,7 +51,7 @@ const RestaurantDetails = ({ user, handleDeleteRestaurant, review }) => {
         <main className={styles.container}>
             <section style={{padding: '0px'}}>
                 <header>
-                    <img src={restaurant.image ? restaurant.image : "../../public/images/default-restaurant-image.jpg"} alt="" />
+                    <img src={restaurant.image ? restaurant.image : "../../public/images/default-restaurant-image.png"} alt="a restaurant logo" />
                     <h1>{restaurant.name}</h1>
                     <>
                     <Link id={styles.editLink} to={`/restaurants/${restaurantId}/edit`}>Edit</Link>
